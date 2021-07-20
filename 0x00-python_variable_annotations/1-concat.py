85c6722efa14de470fb79166a4183dc143366ad5
Sergio Steben Arias Quintero <1638@holbertonschool.com>
2021-07-20 17:39:36 -0500

    Task 1 - Basic Notations Cat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Basic Notations Concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Function that takes a two strings and returns a concatenated string
+
+    Args:
+        str1 (str): First String
+        str2 (str): Second String
+
+    Returns:
+        str: Concatenated String
+    """
+    return str1 + str2
