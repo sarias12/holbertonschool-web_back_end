5b65b9873f8928c7993fed1df13bb93b16b275d0
Sergio Steben Arias Quintero <1638@holbertonschool.com>
2021-07-20 17:52:49 -0500

    Basic Notations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Basic Notations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function that takes a float and returns the string representation
+    of the float.
+
+    Args:
+        n (float): Float number
+
+    Returns:
+        str: string representation of the float
+    """
+    return str(n)
