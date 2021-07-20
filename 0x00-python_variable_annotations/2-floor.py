c024315f1600281417288c6d04c69e1674d213c6
Sergio Steben Arias Quintero <1638@holbertonschool.com>
2021-07-20 17:45:32 -0500

    Task 2 - Basic Notations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Basic Notatiosn - floor
+"""
+
+
+def floor(n: float) -> int:
+    """Function which takes a float and return the floor of the float
+
+    Args:
+        n (float): Number to convert
+    Returns:
+        int: Converted Number
+    """
+    return int(n)
