ecb720443fe4eb543e39eb691c9172a01f314c61
Sergio Steben Arias Quintero <1638@holbertonschool.com>
2021-07-20 17:32:53 -0500

    Task 0 - Basic Notations

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
+"""
+Basic Notations in python
+"""
+
+
 def add(a: float, b: float) -> float:
+    """
+    function that returns the sum of the two floats
+
+    Args:
+        a (float): First number
+        b (float): Second Number
+
+    Returns:
+        float: sum of a and b
+    """
     return a + b
