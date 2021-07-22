719a9f370a25a29a95ec1f796031e8feb637fd70
Sergio Steben Arias Quintero <1638@holbertonschool.com>
2021-07-21 21:14:00 -0500

    Task 1 - Async Comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Async Comprehensions
+"""
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """[summary]
+
+    Returns:
+        List[float]
+    """
+    return [i async for i in async_generator()]
