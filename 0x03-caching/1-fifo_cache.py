f6c8c377125f2b3e740583a2c1cc71b317178007
Sergio Steben Arias Quintero <1638@holbertonschool.com>
2021-07-25 10:53:34 -0500

    Task 1 - FIFO Caching

---
+++ 0x03-caching/1-fifo_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" FIFO caching
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+
+    def __init__(self):
+        """Constructor
+        """
+        super().__init__()
+        self.items = []
+
+    def put(self, key, item):
+        """Add item in the cache with FIFO Algorithm
+
+        Args:
+            key (str): value for item
+            item (str): item for dictionary
+        """
+        if (key is not None and item is not None):
+            if (len(self.items) < self.MAX_ITEMS):
+                self.items.append(key)
+                self.cache_data[key] = item
+            else:
+                discard = self.items.pop(0)
+                self.cache_data.pop(discard)
+                self.cache_data[key] = item
+                print('DISCARD: ' + discard)
+
+    def get(self, key):
+        """Get a item by key
+
+        Args:
+            key (str): key for get value from cache data dictionary
+
+        Returns:
+            Value of key (str):  value from cache data or None
+        """
+        if (key in self.cache_data and key is not None):
+            return self.cache_data[key]
+        return None
