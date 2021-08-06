209355e898cbe95db74576471740141baf0098ed
Sergio Steben Arias Quintero <1638@holbertonschool.com>
2021-08-05 20:11:06 -0500

    Task 3. Auth class

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,41 @@
+"""
+Module for  manage the API authentication.
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Auth class
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """[summary]
+
+        Args:
+            path (str)
+            excluded_paths (List[str])
+
+        Returns:
+            bool
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Flask request object - Authorization Header
+
+        Args:
+            request: Defaults to None.
+
+        Returns:
+            str
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Flask request object - Current User
+
+        Returns:
+            User
+        """
+        return None
