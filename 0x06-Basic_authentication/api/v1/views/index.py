3eeb6bba9a5d8c7bb8816a4381953f3984193b99
Sergio Steben Arias Quintero <1638@holbertonschool.com>
2021-08-05 19:46:36 -0500

    Task 2. Error handler: Forbidden

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> str:
       - Error 401
     """
     raise abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - Error 403
+    """
+    raise abort(403)
